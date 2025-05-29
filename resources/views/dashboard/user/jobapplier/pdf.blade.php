93b0faf6bb991fc03e0a2e5c002a11f371f0cbfa
nipa <nipacse246@gmail.com>
2025-05-28 17:31:31 -0700

    pdf style updated

--- resources/views/dashboard/user/jobapplier/pdf.blade.php
+++ resources/views/dashboard/user/jobapplier/pdf.blade.php
@@ -5,35 +5,75 @@
      <meta charset="UTF-8">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
      <title>Job Applier Details</title>
+
+     <style>
+         .img-logo {
+             width: 250px;
+             height: 250px;
+             border-radius: 100%;
+             float: left;
+             padding-top: 20px;
+         }
+
+         .pdf h4 {
+             text-align: center;
+             font-size: 20px;
+             color: darkred;
+         }
+
+         .pdf hr {
+             width: 100%;
+
+         }
+         .details
+         {
+            padding-top: 30px;
+            padding-left: 40px;
+         }
+
+         .details label {
+             font-size: 18px;
+             padding-left: 50px;
+             font-family: 'Poppins', sans-serif;
+         }
+         .details .details1{
+            padding-bottom: 10px;
+            
+
+         }
+     </style>
  </head>
 
  <body>
-     <h4 class="p-2 text-danger">Job Applier Details</h4>
-     <div class="container">
-         <div class="row d-flex">
+     <div class="pdf">
+         <h4 class="p-2 text-danger">Job Applier Details</h4>
+         <hr>
+     </div>
+     <div class="pdf container">
+         <div class="row">
              <div class="col-lg-6">
-                 <img src="uploads/newjobapplier/{{ $jobapplier->newapplier_img }}" width="675px" height="500px">
+                 <img class="img-logo" src="uploads/newjobapplier/{{ $jobapplier->newapplier_img }}">
              </div>
-             <div class="applier-details col-lg-6 pt-6">
-                 <div class="pt-2">
+             <div class="details col-lg-6 pt-6">
+                 <div class="details1 pt-2">
                      <label for="name">Applier Name : {{ $jobapplier->name }}</label>
                  </div>
-                 <div class="pt-2">
+                 <div class="details1 pt-2">
                      <label for="company_name">Company Name : {{ $jobapplier->company_name }}</label>
                  </div>
-                 <div class="pt-2">
+                 <div class="details1 pt-2">
                      <label for="job_title">Job Title : {{ $jobapplier->job_title }}</label>
                  </div>
-                 <div class="pt-2">
+                 <div class="details1 pt-2">
                      <label for="application_date">Application Date : {{ $jobapplier->application_date }}</label>
                  </div>
-                 <div class="pt-2">
+                 <div class="details1 pt-2">
                      <label for="application_status">Application Status : {{ $jobapplier->application_status }}</label>
                  </div>
-                 <div class="pt-2">
+                 <div class="details1 pt-2">
                      <label for="job_board">Job Board : {{ $jobapplier->job_board }}</label>
                  </div>
-                 <div class="pt-2">
+                 <div class="details1 pt-2">
                      <label for="notes">Notes : {{ $jobapplier->notes }}</label>
                  </div>
              </div>
